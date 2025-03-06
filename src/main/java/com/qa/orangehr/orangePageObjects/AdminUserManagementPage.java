4c776ced764ac38b6cbd229b2a805deeaa0f97c4
AKasachova <alionakosachova@gmail.com>
2025-03-06 09:35:07 +0200

    String fields implementation

--- src/main/java/com/qa/orangehr/orangePageObjects/AdminUserManagementPage.java
+++ src/main/java/com/qa/orangehr/orangePageObjects/AdminUserManagementPage.java
@@ -6,21 +6,23 @@ import com.qa.orangehr.framework.elements.TopBar;
 import com.qa.orangehr.framework.elements.TopBarBreadscrumb;
 import com.qa.orangehr.framework.modules.Table;
 import com.qa.orangehr.framework.pages.BasePage;
-
 import java.util.List;
 
 public class AdminUserManagementPage extends BasePage {
+    private String userRoleDropDownSelector = "//div[contains(@class,'oxd-input-group') and .//label[text()='User Role']]"
+            + "//div[@class='oxd-select-wrapper']";
+    private String recordsFoundTableSelector = "//div[@class='oxd-table']";
 
     public AdminUserManagementPage(Page page) {
         super(page);
     }
 
     private DropDown getUserRoleDropDown() {
-        return new DropDown(page, "//div[contains(@class,'oxd-input-group') and .//label[text()='User Role']]//div[@class='oxd-select-wrapper']");
+        return new DropDown(page, userRoleDropDownSelector);
     }
 
     private Table getRecordsFoundTable(){
-        return new Table(page, "//div[@class='oxd-table']");
+        return new Table(page, recordsFoundTableSelector);
     }
 
     protected TopBar topBar = new TopBar(page,topBarSelector, new TopBarBreadscrumb(page, topBarBreadscrumbSelector));
--- src/main/java/com/qa/orangehr/orangePageObjects/DashboardPage.java
+++ src/main/java/com/qa/orangehr/orangePageObjects/DashboardPage.java
@@ -6,16 +6,16 @@ import com.qa.orangehr.framework.elements.DropDownMenu;
 import com.qa.orangehr.framework.pages.BasePage;
 
 public class DashboardPage extends BasePage {
+    private String dropDownMenuSelector = "//li[contains(@class, 'oxd-userdropdown')]";
 
     public DashboardPage(Page page) {
         super(page);
     }
 
     private DropDownMenu getUserMenu(){
-        return new DropDownMenu(page,
-                "//li[contains(@class, 'oxd-userdropdown')]");
+        return new DropDownMenu(page, dropDownMenuSelector);
     }
-    //will not implement separate wrapper
+
     private Element getOptionInSearch(String option) {
         return new Element(page, String.format("//a//*[text()='%s']", option));
     }
